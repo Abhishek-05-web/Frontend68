da9ee99d3cd24f537fae604197b4d7afdef97173
Abhishek-05-web <abhishek18kr2007@gmail.com>
2026-09-09 19:14:33 +0530

    live-date function added

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateDate(){
+    const now=new Date();
+    const day=now.toLocaleDateString("en-IN",{
+        weekday:"long"
+    });
+
+    const date=now.getDate();
+    const month=now.toLocaleDateString("en-IN",{
+        month:"long"
+    }).toUpperCase();
+
+    document.querySelector(".date").textContent=`${day} · ${date} ${month}`;
+}
+updateDate();
+setInterval(updateDate,60000);
